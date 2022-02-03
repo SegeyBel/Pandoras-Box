1e4aaaaff9de69568c5b5c0fc1b88301361a3ffe
Sergey Belik <belikserg1@gmail.com>
2022-02-03 22:06:10 +0200

    print

---
+++ lesson 2/task2.py
@@ -0,0 +1,5 @@
+print('apple', 'banana', 'orange', sep=',')         # разделяет слова запятая
+print('apple', 'banana', 'orange', sep='-')         # разделяет слова -
+
+print('apple', 'banana', 'orange', end='\tfruit')   # добавил слово"fruit" + табуляция
+
