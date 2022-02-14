6384e95430308d937f3cbddd50e61be8ec6f807b
Sergey Belik <belikserg1@gmail.com>
2022-02-14 22:51:36 +0200

    Create ibm if else.py
    ibm if else correct task

---
+++ lesson 4/ibm if else.py
@@ -0,0 +1,12 @@
+weight = 60
+height = 1.75
+BMI = weight/(height * height)
+lower_bound = 18.5
+upper_bound = 25
+if BMI <= lower_bound :
+    print("Your BMI is Low")
+elif BMI >= upper_bound :
+     print("Your BMI is High")
+else:print("Your BMI is Normal")
+
+
